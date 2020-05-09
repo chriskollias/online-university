858ee556b657364e7d2bac9400ed19477e2b6586
chriskollias <chriskollias500@gmail.com>
2020-05-09 12:58:21 -0700

    fixed typo in app name

--- online_university/asgi.py
+++ online_university/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for online_unversity project.
+ASGI config for online_university project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'online_unversity.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'online_university.settings')
 
 application = get_asgi_application()
--- online_university/urls.py
+++ online_university/urls.py
@@ -1,4 +1,4 @@
-"""online_unversity URL Configuration
+"""online_university URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
