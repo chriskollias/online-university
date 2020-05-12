c8dbf0fd2d17f3729312ae3719ee2ed365d85df5
chriskollias <chriskollias500@gmail.com>
2020-05-12 15:37:21 -0700

    created subject model for course subjects

--- course/models.py
+++ course/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
+class Subject(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.CharField(max_length=250)
+
 class Course(models.Model):
     name = models.CharField(max_length=150)
     description = models.CharField(max_length=250)
     course_code = models.CharField(max_length=8, unique=True)
-    subject = models.CharField(max_length=100)
+    subject = models.ForeignKey(Subject, null=True, on_delete=models.SET_NULL) # how would I do SET_DEFAULT instead?
     syllabus = models.FileField(null=True)
     instructors = models.ManyToManyField(User, related_name='instructors')
     students = models.ManyToManyField(User, related_name='students')
@@ -14,3 +17,6 @@ class Course(models.Model):
 
     def __str__(self):
         return f'{self.name} ({self.course_code})'
+
+
+ 
